62b0e886c765b0df85e229703e6436cf181e12bb
Kyle Lahnakoski <klahnakoski@mozilla.com>
2016-12-30 14:46:21 -0500

    first commit

---
+++ tests/test_dot.py
@@ -0,0 +1,473 @@
+# encoding: utf-8
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this file,
+# You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+# Author: Kyle Lahnakoski (kyle@lahnakoski.com)
+#
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import unicode_literals
+
+from UserDict import UserDict
+from collections import Mapping
+
+from pyLibrary.collections import MAX
+from pyLibrary.debugs.logs import Log
+from pyLibrary.meta import DataClass
+from pyLibrary.testing.fuzzytestcase import FuzzyTestCase
+
+from pyDots import wrap, Null, set_default, unwrap, Data
+from pyDots.objects import datawrap
+
+
+class TestDot(FuzzyTestCase):
+
+    def test_set_union_w_null(self):
+        s = set('a')
+        s |= Null
+        self.assertAlmostEqual(s, set('a'))
+
+
+    def test_null_class(self):
+        self.assertFalse(isinstance(Null, Mapping))
+
+
+    def test_userdict(self):
+        def show_kwargs(**kwargs):
+            return kwargs
+
+        a = UserDict(a=1, b=2)
+        d = show_kwargs(**a)
+        self.assertAlmostEqual(d, {"a":1, "b":2})
+
+    def test__userdict(self):
+        def show_kwargs(**kwargs):
+            return kwargs
+
+        a = _UserDict()
+        a.data["a"] = 1
+        a.data["b"] = 2
+        d = show_kwargs(**a)
+        self.assertAlmostEqual(d, {"a":1, "b":2})
+
+    def test_dict_args(self):
+        def show_kwargs(**kwargs):
+            return kwargs
+
+        a = Data()
+        a["a"] = 1
+        a["b"] = 2
+        d = show_kwargs(**a)
+        self.assertAlmostEqual(d, {"a": 1, "b": 2})
+
+    def test_is_mapping(self):
+        self.assertTrue(isinstance(Data(), Mapping), "All Data must be Mappings")
+
+    def test_none(self):
+        a = 0
+        b = 0
+        c = None
+        d = None
+
+        if a == b:
+            pass
+        else:
+            Log.error("error")
+
+        if c == d:
+            pass
+        else:
+            Log.error("error")
+
+        if a == c:
+            Log.error("error")
+
+        if d == b:
+            Log.error("error")
+
+        if not c:
+            pass
+        else:
+            Log.error("error")
+
+
+    def test_null(self):
+        a = 0
+        b = 0
+        c = Null
+        d = Null
+        e = Data()
+        f = Data()
+
+        if a == b:
+            pass
+        else:
+            Log.error("error")
+
+        if c == d:
+            pass
+        else:
+            Log.error("error")
+
+        if a == c:
+            Log.error("error")
+
+        if d == b:
+            Log.error("error")
+
+        if c == None:
+            pass
+        else:
+            Log.error("error")
+
+        if not c:
+            pass
+        else:
+            Log.error("error")
+
+        if Null != Null:
+            Log.error("error")
+
+        if Null != None:
+            Log.error("error")
+
+        if None != Null:
+            Log.error("error")
+
+        if e.test != f.test:
+            Log.error("error")
+
+    def test_get_value(self):
+        a = wrap({"a": 1, "b": {}})
+
+        if a.a != 1:
+            Log.error("error")
+        if not isinstance(a.b, Mapping):
+            Log.error("error")
+
+    def test_get_class(self):
+        a = wrap({})
+        _type = a.__class__
+
+        if _type is not Data:
+            Log.error("error")
+
+    def test_int_null(self):
+        a = Data()
+        value = a.b*1000
+        assert value == Null
+
+    def test_dot_self(self):
+        a = Data(b=42)
+        assert a["."] == a
+        assert a["."].b == 42
+
+        a["."] = {"c": 42}
+        assert a.c == 42
+        assert a.b == None
+
+
+    def test_list(self):
+        if not []:
+            pass
+        else:
+            Log.error("error")
+
+        if []:
+            Log.error("error")
+
+        if not [0]:
+            Log.error("error")
+
+
+    def test_assign1(self):
+        a = {}
+
+        b = wrap(a)
+        b.c = "test1"
+        b.d.e = "test2"
+        b.f.g.h = "test3"
+        b.f.i = "test4"
+        b.k["l.m.n"] = "test5"
+
+        expected = {
+            "c": "test1",
+            "d": {
+                "e": "test2"
+            },
+            "f": {
+                "g": {
+                    "h": "test3"
+                },
+                "i": "test4"
+            },
+            "k": {
+                "l": {"m": {"n": "test5"}}
+            }
+        }
+        self.assertEqual(a, expected)
+
+
+    def test_assign2(self):
+        a = {}
+
+        b = wrap(a)
+        b_c = b.c
+        b.c.d = "test1"
+
+        b_c.e = "test2"
+
+        expected = {
+            "c": {
+                "d": "test1",
+                "e": "test2"
+            }
+        }
+        self.assertEqual(a, expected)
+
+    def test_assign3(self):
+        # IMPOTENT ASSIGNMENTS DO NOTHING
+        a = {}
+        b = wrap(a)
+
+        b.c = None
+        expected = {}
+        self.assertEqual(a, expected)
+
+        b.c.d = None
+        expected = {}
+        self.assertEqual(a, expected)
+
+        b["c.d"] = None
+        expected = {}
+        self.assertEqual(a, expected)
+
+        b.c.d.e = None
+        expected = {}
+        self.assertEqual(a, expected)
+
+        b.c["d.e"] = None
+        expected = {}
+        self.assertEqual(a, expected)
+
+    def test_assign4(self):
+        # IMPOTENT ASSIGNMENTS DO NOTHING
+        a = {"c": {"d": {}}}
+        b = wrap(a)
+        b.c.d = None
+        expected = {"c": {}}
+        self.assertEqual(a, expected)
+
+        a = {"c": {"d": {}}}
+        b = wrap(a)
+        b.c = None
+        expected = {}
+        self.assertEqual(a, expected)
+
+
+    def test_assign5(self):
+        a = {}
+        b = wrap(a)
+
+        b.c["d\.e"].f = 2
+        expected = {"c": {"d.e": {"f": 2}}}
+        self.assertEqual(a, expected)
+
+    def test_assign6(self):
+        a = {}
+        b = wrap(a)
+
+        b["c.d.e\.f"] = 1
+        b["c.d.e\.g"] = 2
+
+        expected = {"c": {"d": {"e.f": 1, "e.g": 2}}}
+        self.assertEqual(a, expected)
+
+    def test_assign7(self):
+        a = {}
+        b = wrap(a)
+
+        b["c.d.e\.f"] = 1
+        b["c.d.g\.h"] = 2
+
+        expected = {"c": {"d": {"e.f": 1, "g.h": 2}}}
+        self.assertEqual(a, expected)
+
+    def test_setitem_and_deep(self):
+        a = {}
+        b = wrap(a)
+
+        b.c["d"].e.f = 3
+        expected = {"c": {"d": {"e": {"f": 3}}}}
+        self.assertEqual(a, expected)
+
+    def test_assign_and_use1(self):
+        a = wrap({})
+        agg = a.b
+        agg.c = []
+        agg.c.append("test value")
+
+        self.assertEqual(a, {"b": {"c": ["test value"]}})
+        self.assertEqual(a.b, {"c": ["test value"]})
+        self.assertEqual(a.b.c, ["test value"])
+
+    def test_assign_and_use2(self):
+        a = wrap({})
+        agg = a.b.c
+        agg += []
+        agg.append("test value")
+
+        self.assertEqual(a, {"b": {"c": ["test value"]}})
+        self.assertEqual(a.b, {"c": ["test value"]})
+        self.assertEqual(a.b.c, ["test value"])
+
+
+    def test_increment(self):
+        a = {}
+        b = wrap(a)
+        b.c1.d += 1
+        b.c2.e += "e"
+        b.c3.f += ["f"]
+        b["c\\.a"].d += 1
+
+        self.assertEqual(a,  {"c1": {"d": 1}, "c2": {"e": "e"}, "c3": {"f": ["f"]}, "c.a": {"d": 1}})
+
+        b.c1.d += 2
+        b.c2.e += "f"
+        b.c3.f += ["g"]
+        b["c\\.a"].d += 3
+        self.assertEqual(a,  {"c1": {"d": 3}, "c2": {"e": "ef"}, "c3": {"f": ["f", "g"]}, "c.a": {"d": 4}})
+
+    def test_slicing(self):
+
+        def diff(record, index, records):
+            """
+            WINDOW FUNCTIONS TAKE THE CURRENT record, THE index THAT RECORD HAS
+            IN THE WINDOW, AND THE (SORTED) LIST OF ALL records
+            """
+            # COMPARE CURRENT VALUE TO MAX OF PAST 5, BUT NOT THE VERY LAST ONE
+            try:
+                return record - MAX(records[index - 6:index - 1:])
+            except Exception, e:
+                return None
+
+
+        data1_list = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+        result1 = [diff(r, i, data1_list) for i, r in enumerate(data1_list)]
+        assert result1 == [-7, None, None, None, None, None, 2, 2, 2]  # WHAT IS EXPECTED, BUT NOT WHAT WE WANT
+
+        data2_list = wrap(data1_list)
+        result2 = [diff(r, i, data2_list) for i, r in enumerate(data2_list)]
+        assert result2 == [None, None, 2, 2, 2, 2, 2, 2, 2]
+
+    def test_delete1(self):
+        a = wrap({"b": {"c": 1}})
+
+        del a.b.c
+        self.assertEqual({}, a)
+        self.assertEqual(a, {})
+
+        a = wrap({"b": {"c": 1}})
+
+        a.b.c=None
+        self.assertEqual({}, a)
+        self.assertEqual(a, {})
+
+
+
+    def test_delete2(self):
+        a = wrap({"b": {"c": 1, "d": 2}})
+
+        del a.b.c
+        self.assertEqual({"b": {"d": 2}}, a)
+        self.assertEqual(a, {"b": {"d": 2}})
+        a = wrap({"b": {"c": 1, "d": 2}})
+
+        a.b.c=None
+        self.assertEqual({"b": {"d": 2}}, a)
+        self.assertEqual(a, {"b": {"d": 2}})
+
+
+    def test_object_wrap(self):
+        d = SampleData()
+        dd = datawrap(d)
+
+        self.assertEqual(dd["a"], 20)
+        self.assertEqual(dd, {"a": 20, "b": 30})
+
+    def test_deep_select(self):
+        d = wrap([{"a": {"b": 1}}, {"a": {"b": 2}}])
+
+        test = d.a.b
+        self.assertEqual(test, [1, 2])
+
+    def test_deep_select_list(self):
+        d = wrap({"a": {"b": [{"c": 1}, {"c": 2}]}})
+
+        test = d["a.b.c"]
+        self.assertEqual(test, [1, 2])
+
+    def test_set_default(self):
+        a = {"x": {"y": 1}}
+        b = {"x": {"z": 2}}
+        c = {}
+        d = set_default(c, a, b)
+
+        self.assertTrue(unwrap(d) is c, "expecting first parameter to be returned")
+        self.assertEqual(d.x.y, 1, "expecting d to have attributes of a")
+        self.assertEqual(d.x.z, 2, "expecting d to have attributes of b")
+
+        self.assertEqual(wrap(a).x.z, None, "a should not have been altered")
+
+    def test_Dict_of_Dict(self):
+        value = {"a": 1}
+        wrapped = Data(Data(value))
+        self.assertTrue(value is unwrap(wrapped), "expecting identical object")
+
+
+    def test_leaves_of_mappings(self):
+        a = wrap({"a": _TestMapping()})
+        a.a.a = {"a": 1}
+        a.a.b = {"b": 2}
+
+        leaves = wrap(dict(a.leaves()))
+        self.assertEqual(a.a.a['a'], leaves["a\.a\.a"], "expecting 1")
+        self.assertEqual(a.a.b['b'], leaves["a\.b\.b"], "expecting 2")
+
+
+
+_TestMapping = DataClass("_TestMapping", ["a", "b"])
+
+
+
+
+
+
+
+class _UserDict:
+    """
+    COPY OF UserDict
+    """
+    def __init__(self, **kwargs):
+        self.data = {}
+    def __getitem__(self, key):
+        if key in self.data:
+            return self.data[key]
+        if hasattr(self.__class__, "__missing__"):
+            return self.__class__.__missing__(self, key)
+        raise KeyError(key)
+    def keys(self):
+        return self.data.keys()
+
+
+class SampleData(object):
+
+    def __init__(self):
+        self.a = 20
+        self.b = 30
+
+    def __str__(self):
+        return str(self.a)+str(self.b)
---
+++ tests/test_dot_from_addict.py
@@ -0,0 +1,357 @@
+# encoding: utf-8
+#
+# DEC 2016 - Altered tests to work with differing definitions
+#
+# SNAGGED FROM https://github.com/mewwts/addict/blob/62e8481a2a5c8520259809fc306698489975c9f8/test_addict.py WITH THE FOLLOWING LICENCE
+#
+# The MIT License (MIT)
+#
+# Copyright (c) 2014 Mats Julian Olsen
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+from __future__ import unicode_literals
+
+from collections import Mapping
+from copy import copy, deepcopy
+import json
+import pickle
+from unittest import skipIf
+
+from pyLibrary import convert
+from pyDots import Data, set_default, unwrap
+from pyLibrary.testing.fuzzytestcase import FuzzyTestCase
+
+TEST_VAL = [1, 2, 3]
+TEST_DICT = {'a': {'b': {'c': TEST_VAL}}}
+
+
+class AddictTests(FuzzyTestCase):
+
+    def test_set_one_level_item(self):
+        some_dict = {'a': TEST_VAL}
+        prop = Data()
+        prop['a'] = TEST_VAL
+        self.assertEqual(prop, some_dict)
+
+    def test_set_two_level_items(self):
+        some_dict = {'a': {'b': TEST_VAL}}
+        prop = Data()
+        prop['a']['b'] = TEST_VAL
+        self.assertEqual(prop, some_dict)
+
+    def test_set_three_level_items(self):
+        prop = Data()
+        prop['a']['b']['c'] = TEST_VAL
+        self.assertEqual(prop, TEST_DICT)
+
+    def test_set_one_level_property(self):
+        prop = Data()
+        prop.a = TEST_VAL
+        self.assertEqual(prop, {'a': TEST_VAL})
+
+    def test_set_two_level_properties(self):
+        prop = Data()
+        prop.a.b = TEST_VAL
+        self.assertEqual(prop, {'a': {'b': TEST_VAL}})
+
+    def test_set_three_level_properties(self):
+        prop = Data()
+        prop.a.b.c = TEST_VAL
+        self.assertEqual(prop, TEST_DICT)
+
+    def test_init_with_dict(self):
+        self.assertEqual(TEST_DICT, Data(TEST_DICT))
+
+    def test_init_with_kws(self):
+        prop = Data(a=2, b={'a': 2}, c=[{'a': 2}])
+        self.assertEqual(prop, {'a': 2, 'b': {'a': 2}, 'c': [{'a': 2}]})
+
+    def test_init_with_list(self):
+        prop = Data([('0', 1), ('1', 2), ('2', 3)])
+        self.assertEqual(prop, {'0': 1, '1': 2, '2': 3})
+
+    def test_init_raises(self):
+        def init():
+            Data(5)
+
+        def init2():
+            Data('a')
+        self.assertRaises(Exception, init)
+        self.assertRaises(Exception, init2)
+
+    def test_init_with_empty_stuff(self):
+        a = Data({})
+        b = Data([])
+        self.assertEqual(a, {})
+        self.assertEqual(b, {})
+
+    def test_init_with_list_of_dicts(self):
+        a = Data({'a': [{'b': 2}]})
+        self.assertIsInstance(a.a[0], Data)
+        self.assertEqual(a.a[0].b, 2)
+
+    def test_getitem(self):
+        prop = Data(TEST_DICT)
+        self.assertEqual(prop['a']['b']['c'], TEST_VAL)
+
+    def test_empty_getitem(self):
+        prop = Data()
+        prop.a.b.c
+        self.assertEqual(prop, {})
+
+    def test_getattr(self):
+        prop = Data(TEST_DICT)
+        self.assertEqual(prop.a.b.c, TEST_VAL)
+
+    def test_isinstance(self):
+        self.assertTrue(isinstance(Data(), Mapping))
+
+    def test_str(self):
+        prop = Data(TEST_DICT)
+        self.assertEqual(str(prop), str(TEST_DICT))
+
+    def test_json(self):
+        some_dict = TEST_DICT
+        some_json = convert.value2json(some_dict)
+        prop = Data()
+        prop.a.b.c = TEST_VAL
+        prop_json = convert.value2json(prop)
+        self.assertEqual(some_json, prop_json)
+
+    def test_delitem(self):
+        prop = Data({'a': 2})
+        del prop['a']
+        self.assertEqual(prop, {})
+
+    def test_delitem_nested(self):
+        prop = Data(deepcopy(TEST_DICT))
+        del prop['a']['b']['c']
+        self.assertEqual(prop, {'a': {'b': {}}})
+
+    def test_delattr(self):
+        prop = Data({'a': 2})
+        del prop.a
+        self.assertEqual(prop, {})
+
+    def test_delattr_nested(self):
+        prop = Data(deepcopy(TEST_DICT))
+        del prop.a.b.c
+        self.assertEqual(prop, {'a': {'b': {}}})
+
+    def test_delitem_delattr(self):
+        prop = Data(deepcopy(TEST_DICT))
+        del prop.a['b']
+        self.assertEqual(prop, {'a': {}})
+
+    def test_set_prop_invalid(self):
+        prop = Data()
+        prop.keys = 2
+        prop.items = 3
+
+        self.assertEqual(prop, {'keys': 2, 'items': 3})
+
+    def test_dir(self):
+        key = 'a'
+        prop = Data({key: 1})
+        dir_prop = dir(prop)
+
+        dir_dict = dir(Data)
+        for d in dir_dict:
+            self.assertTrue(d in dir_prop, d)
+
+        self.assertTrue('__methods__' not in dir_prop)
+        self.assertTrue('__members__' not in dir_prop)
+
+    def test_dir_with_members(self):
+        prop = Data({'__members__': 1})
+        dir(prop)
+        self.assertTrue('__members__' in prop.keys())
+
+    def test_unwrap(self):
+        nested = {'a': [{'a': 0}, 2], 'b': {}, 'c': 2}
+        prop = Data(nested)
+        regular = unwrap(prop)
+        self.assertEqual(regular, prop)
+        self.assertEqual(regular, nested)
+        self.assertNotIsInstance(regular, Data)
+
+        def get_attr():
+            regular.a = 2
+        self.assertRaises(AttributeError, get_attr)
+
+        def get_attr_deep():
+            regular['a'][0].a = 1
+        self.assertRaises(AttributeError, get_attr_deep)
+
+    def test_to_dict_with_tuple(self):
+        nested = {'a': ({'a': 0}, {2: 0})}
+        prop = Data(nested)
+        regular = unwrap(prop)
+        self.assertEqual(regular, prop)
+        self.assertEqual(regular, nested)
+        self.assertIsInstance(regular['a'], tuple)
+        self.assertNotIsInstance(regular['a'][0], Data)
+
+    def test_update(self):
+        old = Data()
+        old.child.a = 'a'
+        old.child.b = 'b'
+        old.foo = 'c'
+
+        new = Data()
+        new.child.b = 'b2'
+        new.child.c = 'c'
+        new.foo.bar = True
+
+        old = set_default({}, new, old)
+
+        reference = {'foo': {'bar': True},
+                     'child': {'a': 'a', 'c': 'c', 'b': 'b2'}}
+
+        self.assertEqual(old, reference)
+
+    def test_update_with_lists(self):
+        org = Data()
+        org.a = [1, 2, {'a': 'superman'}]
+        someother = Data()
+        someother.b = [{'b': 123}]
+        org.update(someother)
+
+        correct = {'a': [1, 2, {'a': 'superman'}],
+                   'b': [{'b': 123}]}
+
+        org.update(someother)
+        self.assertEqual(org, correct)
+        self.assertIsInstance(org.b[0], Mapping)
+
+    def test_update_with_kws(self):
+        org = Data(one=1, two=2)
+        someother = Data(one=3)
+        someother.update(one=1, two=2)
+        self.assertEqual(org, someother)
+
+    def test_update_with_args_and_kwargs(self):
+        expected = {'a': 1, 'b': 2}
+        org = Data()
+        org.update({'a': 3, 'b': 2}, a=1)
+        self.assertEqual(org, expected)
+
+    def test_update_with_multiple_args(self):
+        org = Data()
+        def update():
+            org.update({'a': 2}, {'a': 1})
+        self.assertRaises(TypeError, update)
+
+    def test_hook_in_constructor(self):
+        a_dict = Data(TEST_DICT)
+        self.assertIsInstance(a_dict['a'], Data)
+
+    def test_copy(self):
+        class MyMutableObject(object):
+
+            def __init__(self):
+                self.attribute = None
+
+        foo = MyMutableObject()
+        foo.attribute = True
+
+        a = Data()
+        a.child.immutable = 42
+        a.child.mutable = foo
+
+        b = a.copy()
+
+        # immutable object should not change
+        b.child.immutable = 21
+        self.assertEqual(a.child.immutable, 21)
+
+        # mutable object should change
+        b.child.mutable.attribute = False
+        self.assertEqual(a.child.mutable.attribute, b.child.mutable.attribute)
+
+        # changing child of b should not affect a
+        b.child = "new stuff"
+        self.assertTrue(isinstance(a.child, Data))
+
+    def test_deepcopy(self):
+        class MyMutableObject(object):
+            def __init__(self):
+                self.attribute = None
+
+        foo = MyMutableObject()
+        foo.attribute = True
+
+        a = Data()
+        a.child.immutable = 42
+        a.child.mutable = foo
+
+        b = deepcopy(a)
+
+        # immutable object should not change
+        b.child.immutable = 21
+        self.assertEqual(a.child.immutable, 42)
+
+        # mutable object should not change
+        b.child.mutable.attribute = False
+        self.assertTrue(a.child.mutable.attribute)
+
+        # changing child of b should not affect a
+        b.child = "new stuff"
+        self.assertTrue(isinstance(a.child, Data))
+
+    def test_add_on_empty_dict(self):
+        d = Data()
+        d.x.y += 1
+
+        self.assertEqual(d.x.y, 1)
+
+    def test_add_on_non_empty_dict(self):
+        d = Data()
+        d.x.y = 'defined'
+
+        def run():
+            d.x += 1
+
+        self.assertRaises(TypeError, run)
+
+    def test_add_on_non_empty_value(self):
+        d = Data()
+        d.x.y = 1
+        d.x.y += 1
+
+        self.assertEqual(d.x.y, 2)
+
+    def test_add_on_unsupported_type(self):
+        d = Data()
+        d.x.y = 'str'
+
+        def test():
+            d.x.y += 1
+
+        self.assertRaises(TypeError, test)
+
+    def test_init_from_zip(self):
+        keys = ['a']
+        values = [42]
+        items = zip(keys, values)
+        d = Data(items)
+        self.assertEqual(d.a, 42)
+
+
