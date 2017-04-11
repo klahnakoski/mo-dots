1ec63c0d509df13273ff1e59951f568a5e4c6112
Kyle Lahnakoski <klahnakoski@mozilla.com>
2017-04-11 16:32:54 -0400

    update version number

--- setup.py
+++ setup.py
@@ -44,7 +44,7 @@ def find_packages(where='.', lib_prefix='', exclude=()):
 
 setup(
     name='mo-dots',
-    version="1.3.17085",
+    version="1.3.17101",
     description='More Dots! Dot-access to Python dicts like Javascript',
     long_description=long_desc,
     author='Kyle Lahnakoski',
