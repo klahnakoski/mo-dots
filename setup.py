4f7620c4b30d2d31aa315770de7eea0d46b2a46c
Kyle Lahnakoski <kyle@lahnakoski.com>
2021-06-30 21:46:04 -0400

    update version number

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 setup(
     author='Kyle Lahnakoski',
     author_email='kyle@lahnakoski.com',
-    classifiers=["Development Status :: 4 - Beta","Topic :: Software Development :: Libraries","Topic :: Software Development :: Libraries :: Python Modules","License :: OSI Approved :: Mozilla Public License 2.0 (MPL 2.0)","Programming Language :: Python :: 2.7","Programming Language :: Python :: 3.7"],
+    classifiers=["Development Status :: 4 - Beta","Topic :: Software Development :: Libraries","Topic :: Software Development :: Libraries :: Python Modules","License :: OSI Approved :: Mozilla Public License 2.0 (MPL 2.0)","Programming Language :: Python :: 3.7","Programming Language :: Python :: 3.9"],
     description='More Dots! Dot-access to Python dicts like Javascript',
     include_package_data=True,
     install_requires=["mo-future==3.147.20327","mo-imports==5.12.21182"],
@@ -15,6 +15,6 @@ setup(
     name='mo-dots',
     packages=["mo_dots"],
     url='https://github.com/klahnakoski/mo-dots',
-    version='5.13.21182',
+    version='5.14.21182',
     zip_safe=False
 )
