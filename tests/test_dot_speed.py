15bce5bf4e2aec1102c167ab4894e5e82c845eea
Kyle Lahnakoski <kyle@lahnakoski.com>
2022-01-20 19:18:06 -0500

    fix tests

--- tests/test_dot_speed.py
+++ tests/test_dot_speed.py
@@ -146,16 +146,15 @@ class TestDotSpeed(FuzzyTestCase):
         self.assertEqual(e_result, i_result)
         self.assertEqual(n_result, i_result)
 
-        # TOO CLOSE TO TEST
-        # self.assertGreater(
-        #     i_time.duration.total_seconds(),
-        #     s_time.duration.total_seconds(),
-        #     msg="isinstance should be slower than __class__ in set",
-        # )
+        self.assertGreater(
+            i_time.duration.total_seconds(),
+            s_time.duration.total_seconds() * 1.1,
+            msg="isinstance should be slower than __class__ in set",
+        )
 
         self.assertGreater(
             m_time.duration.total_seconds(),
-            s_time.duration.total_seconds(),
+            s_time.duration.total_seconds()*1.1,
             "is_text should be slower than isinstance check",
         )
 
