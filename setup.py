f7ecb671499f5059681941670d67a148838a0d98
Kyle Lahnakoski <kyle@lahnakoski.com>
2022-02-08 08:15:26 -0500

    update version number

--- setup.py
+++ setup.py
@@ -16,6 +16,6 @@ setup(
     name='mo-dots',
     packages=["mo_dots"],
     url='https://github.com/klahnakoski/mo-dots',
-    version='9.131.22039',
+    version='9.132.22039',
     zip_safe=False
 )
