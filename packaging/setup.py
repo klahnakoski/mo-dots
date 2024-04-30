08ad459e0a135b315450bb7d172d561e10c795de
Kyle Lahnakoski <kyle@lahnakoski.com>
2024-04-29 21:06:24 -0400

    update version number

--- packaging/setup.py
+++ packaging/setup.py
@@ -15,6 +15,6 @@ setup(
     name='mo-dots',
     packages=["mo_dots"],
     url='https://github.com/klahnakoski/mo-dots',
-    version='10.612.24121',
+    version='10.613.24121',
     zip_safe=False
 )
